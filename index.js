3079cc221427c0abc0d8d5766ab3cffa10637120
miracleuba <miracleuba73@gmail.com>
2025-07-09 13:08:56 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent actual form submission
+      alert('Submission successful');
+    });
